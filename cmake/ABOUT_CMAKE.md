083b7986a3df87d051d37cfd55a04c19235c44f5
David C Black <david.black@doulos.com>
2020-10-13 02:39:30 -0500

    Compatibility with SystemC Complete project

---
+++ cmake/ABOUT_CMAKE.md
@@ -0,0 +1,8 @@
+About cmake
+===========
+
+Cmake is a platform independent tool for creating build environments. It
+supports traditional Unix make as well as Windows Visual Studio, Mac XCode,
+and Eclipse.
+
+Obtain cmake, documentation and tutorial information from <https://cmake.org>.
---
+++ cmake/SystemC.cmake
@@ -0,0 +1,22 @@
+#!cmake .
+
+# SystemC specific needs
+set(CMAKE_PREFIX_PATH $ENV{SYSTEMC_HOME})
+find_package(SystemCLanguage CONFIG REQUIRED)
+set (CMAKE_CXX_STANDARD ${SystemC_CXX_STANDARD} CACHE STRING
+     "C++ standard to build all targets. Supported values are 98, 11, and 14.")
+set (CMAKE_CXX_STANDARD_REQUIRED ${SystemC_CXX_STANDARD_REQUIRED} CACHE BOOL
+     "The with CMAKE_CXX_STANDARD selected C++ standard is a requirement.")
+link_libraries(SystemC::systemc)
+
+# Things that affect all targets
+set(shared_env LD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib DYLD_LIBRARY_PATH=${CMAKE_PREFIX_PATH}/lib)
+
+#find_package( Boost 1.65 REQUIRED )
+#find_package( yaml-cpp CONFIG REQUIRED )
+
+add_custom_target(run
+   time env ${shared_env} ./${TARGET_EXECUTABLE} ${ARGS} 2>&1 | tee ${TARGET_EXECUTABLE}.log
+   COMMENT "Running program ${TARGET_EXECUTABLE}"
+   USES_TERMINAL
+   )
