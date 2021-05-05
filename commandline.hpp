4bbbfccfbb6711b276a5c1e634e05a7c44f59ecf
David C Black <david.black@doulos.com>
2021-05-05 16:16:28 -0500

    Add some documentation and corrected a pass-by-value to be pass-by-reference

--- commandline.hpp
+++ commandline.hpp
@@ -3,12 +3,23 @@
 #include "systemc.hpp"
 #include <string>
 
+/* Usage: #include this file in your design. Then call Commandline::has_opt("-YOUR_SWITCH").
+ * The return value will be zero if not found; otherwise, it will represent which
+ * command-line argument matched. For example:
+     // Following is C++17 code..
+     bool debug=false;
+     if ( int i = Commandline::has_opt("--debug"); i != 0 ) {
+        DEBUG( "Enabling debug" );
+        debug=true;
+     }
+ */
+
 struct Commandline
 {
   // Return index if a command-line argument beginning with opt exists; otherwise, zero
   // Optional index may be used to continue scanning from previous scan (0 =>
   // start from first)
-  inline static size_t has_opt( std::string opt, int i=0 )
+  inline static size_t has_opt( const std::string& opt, int i=0 )
   {
     for( ++i; i < sc_core::sc_argc(); ++i ) {
       std::string arg{ sc_core::sc_argv()[ i ] };
