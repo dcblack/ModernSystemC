dd73047c589be1d780420c406097e86b9dece9fb
David C Black <david.black@doulos.com>
2021-05-05 16:19:52 -0500

    Improve documentation to support doxygen

--- wallclock.hpp
+++ wallclock.hpp
@@ -1,13 +1,15 @@
 #pragma once
 
-#include <cstdint>
-
-// get_wall_time() returns wall wall clock time since epoch in seconds - reasonable for simulator
+///
+// @brief get elapsed actual wall clock time - reasonable for simulator
 // performance measurements. Works for Linux, OS X and Windows.
+// @return wall clock time since epoch in seconds
 double get_wall_time(void);
 
-// get_cpu_time() returns wall CPU clock time since start of program in seconds - reasonable for simulator
+///
+// @brief get elapsed CPU time since program start - reasonable for simulator
 // performance measurements. Does not include sub-processes (i.e. system() calls).
 // Works for Linux, OS X and Windows.
+// @return returns CPU clock time since start of program in seconds
 double get_cpu_time(void);
 //EOF
